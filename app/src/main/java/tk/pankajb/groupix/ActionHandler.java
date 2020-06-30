e9cdc046a852582851ff62902bb525da49b6ad82
Pankaj Bairwa <bairwapankaj25@gmail.com>
2020-06-30 21:28:05 +0530

    Album Image Download/Delete bug fixes

--- app/src/main/java/tk/pankajb/groupix/ActionHandler.java
+++ app/src/main/java/tk/pankajb/groupix/ActionHandler.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FileDownloadTask;
 
 import java.io.File;
@@ -26,18 +29,14 @@ public class ActionHandler {
     public void DownloadSingleImage(final String ImageId, String OwnerId, final Context context) {
 
         File f = new File("/sdcard/Groupix/");
-
         if (!f.exists()) {
             f.mkdir();
         }
-
         TemporaryFile = new File("/sdcard/Groupix/", ImageId + ".jpg");
-
         AppData.getImagesStorageRef().child(OwnerId).child(ImageId).child("image").getFile(TemporaryFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                 Log.i("ImageDownload", "Image Downloaded " + ImageId + ".jpg to " + "Image downloaded to " + Environment.getExternalStorageDirectory() + "/" + R.string.app_name);
-
                 Toast.makeText(context, "Image downloaded to " + Environment.getExternalStorageDirectory() + "/" + R.string.app_name, Toast.LENGTH_LONG).show();
 
             }
@@ -51,8 +50,40 @@ public class ActionHandler {
         });
     }
 
-    public void DownloadSingleImage(String AlbumId, String ImageId, String OwnerId, Context context) {
-        Toast.makeText(context, "Download Album Image Clicked", Toast.LENGTH_LONG).show();
+    public void DownloadSingleImage(final String AlbumId, final String ImageId, final String OwnerId, final Context context) {
+        File f = new File("/sdcard/Groupix/");
+        if (!f.exists()) {
+            f.mkdir();
+        }
+
+        AppData.getAlbumsDataRef().child(OwnerId).child(AlbumId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                File f1 = new File("/sdcard/Groupix/" + (String) dataSnapshot.child("name").getValue() + "/");
+                if (!f1.exists()) {
+                    f1.mkdir();
+                }
+                TemporaryFile = new File("/sdcard/Groupix/" + (String) dataSnapshot.child("name").getValue() + "/", ImageId + ".jpg");
+
+                AppData.getAlbumsStorageRef().child(OwnerId).child(AlbumId).child("images").child(ImageId).child("image").getFile(TemporaryFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Log.i("ImageDownload", "Image Downloaded " + ImageId + ".jpg to " + "Image downloaded to " + Environment.getExternalStorageDirectory() + "/" + R.string.app_name);
+                        Toast.makeText(context, "Image downloaded to " + Environment.getExternalStorageDirectory() + "/" + R.string.app_name, Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("ImageDownload", "Image not downloaded with error code " + e);
+                        Toast.makeText(context, "Image not downloaded successfully", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     public void DeleteSingleImage(String ImageId, String OwnerId) {
@@ -67,7 +98,7 @@ public class ActionHandler {
         AppData.getAlbumsDataRef().child("allimages").child(ImageId).setValue(null);
         AppData.getAlbumsDataRef().child(OwnerId).child(AlbumId).child("images").child(ImageId).setValue(null);
 
-        AppData.getAlbumsStorageRef().child(OwnerId).child(AlbumId).child("images").child(ImageId).delete();
+        AppData.getAlbumsStorageRef().child(OwnerId).child(AlbumId).child("images").child(ImageId).child("image").delete();
     }
 
     void DisplaySingleImage(Context currentContext, String ImageId) {
